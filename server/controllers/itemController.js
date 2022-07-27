08c403fdebc15e89badee64a53c0b1b57995e2c9
lyo-n <1y0mmmm@gmail.com>
2022-07-27 17:16:38 +0300

    findOne

--- server/controllers/itemController.js
+++ server/controllers/itemController.js
@@ -24,36 +24,66 @@ class ItemController {
 
     async getAll (req, res) {
         const {stars, title} = req.query
-        let items;
-        if (!stars && !title) {
-            items = await Item.findAndCountAll()
+    //////ПРАЦЮЮЧЕ
+        // let items;        
+        //     items = await Item.findAndCountAll()             
+        //     return res.json(items)
+
+
+        let itemForTitle = await Item.findAndCountAll()
+        let rows = {...itemForTitle}
+        for (let i = 0; i < rows.rows.length; i++) { 
+            // // console.log(rows.rows[i].dataValues) 
+            // if (rows.rows[i].dataValues.title === null){
+            //     next();
+            // }
+            if (rows.rows[i].dataValues.title.includes(req.body.title) !== null) { 
+                console.log('fuck yea')//////////////////////////////////DELEEEETEEEEE
+                console.log(i)
+                let result = rows.rows.filter(item => item.dataValues.title != req.body.title)
+                return res.json(result)      
+        } else {
+            console.log("fuck u")//////////////////////////////////DELEEEETEEEEE
         }
-        // if (title) {
-        //     items = await Item.findAndCountAll({where:{title}, raw: true })
-        // }
-        // if (stars) {
-        //     items = await Item.findAndCountAll({where:{stars}, raw: true })
-        // }
-        // if (title && stars) {
-        //     items = await Item.findAndCountAll({where:{stars, title}, raw: true })
-        // }
-        return res.json(items)
+            // const resultTitle = (rows.rows[i].dataValues);
+            
+        }
     }
 
 
 
     async getOneElement (req, res) {
-        const {title, stars} =  req.body
-        if (title){
-        const item = await Item.findOne({where: {title}})
-        return res.json(item)
-        } else if (stars) {
-            // const item = await Item.findOne({where: {stars}})
-            const item = await Item.findOne({where: {stars}[stars.req.body]})
-            return res.json(item)
-        }
-    }
+        let actors = getAll(items)
 
+        if (title) {
+            // let itemForTitle = await Item.findAndCountAll()
+            for (let i = 0; i < actors.rows.length; i++) { 
+                if (ractors.rows[i].title.includes(req.body)) { 
+                    console.log(actors.rows[i]) 
+                }
+            }
+            return res.json(itemForTitle)
+
+
+
+/////Потрібно!!!
+
+        // let {title} =  req.body
+        // let {stars} = req.body
+        // if (title){
+        // const item = await Item.findOne({where: {title}})
+        // return res.json(item)
+        // } else if (stars) {
+        //     // const item = await Item.findOne({where: {stars}}
+        //     const item = await Item.findOne({where: {stars}})
+            
+        //     // console.log({stars})
+        //     return res.json(item)
+            
+            
+        // }
+    }
+}
 
 
 
