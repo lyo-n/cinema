4a56bb9cfdd019c7fe5129b39c6a68e9fd4ab0ed
lyo-n <1y0mmmm@gmail.com>
2022-08-01 01:54:45 +0300

    v

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,13 +2,11 @@ const Router = require('express')
 const router = new Router()
 const userRouter = require('./userRouter')
 const itemRouter = require('./itemRouter')
-// const uploadRouter = require('./uploadRouter')
 
 
 
 router.use('/user', userRouter)
 router.use('/item', itemRouter)
-// router.use('/upload', uploadRouter)
 
 
 
